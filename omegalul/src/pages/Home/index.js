a75529b0165e11b7b0af542d79056ea77006f3c1
goldenxuett <ttanpcs@gmail.com>
2020-08-08 17:23:07 -0500

    Implement placeholder website from template

--- omegalul/src/pages/Home/index.js
+++ omegalul/src/pages/Home/index.js
@@ -1,16 +1,75 @@
 import React from "react";
-import { withRouter } from 'react-router-dom';
-import "./style.sass"
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-class Home extends React.Component {
+
+import { faBath, faQuestion, faTruck, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import "./style.sass"
+import sampleHomePage from '..\\..\\assets\\kekw.png'; // Delete Later
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      info: [
+        {
+          "icon": faBath,
+          "title": "What",
+          "content": "5HEAD.",
+        },
+        {
+          "icon": faQuestion,
+          "title": "Why",
+          "content": "KEKW.",
+        },
+        {
+          "icon": faTruck,
+          "title": "How",
+          "content": "OMEGALUL.",
+        },
+      ]
+    }
+  }
+
+
+
   render() {
     return (
       <div className="home-page">
-        
+        <div className="header">
+          <div className="menu">
+            <div className="spacer"/>
+          </div>
+
+          <div className="container">
+
+            <div className="left">
+              <div className="title">OMEGALUL</div>
+              <div className="description">Brought to you by KRISTEN ONE</div>
+            </div>
+            <div className="right">
+              <img className="preview-image" src={sampleHomePage}/>
+            </div> 
+          </div>
+
+          <div className="details-button" onClick={() => window.scroll({left: 0, top: window.innerHeight, behavior: 'smooth'})}>
+            <div className="text">more info</div>
+            <FontAwesomeIcon icon={faChevronDown}/>
+          </div>
+        </div>
+
+        <div className="info-section">
+          {
+            this.state.info.map(({ icon, title, content }) => (
+              <div className="info" key={title}>
+                <FontAwesomeIcon className="icon" icon={icon}/>
+                <div className="title">{title}</div>
+                <div className="content">{content}</div>
+              </div>
+            ))
+          }
+        </div>
       </div>
-      )
+    )
   }
 }
-
-
-export default withRouter(Home)
