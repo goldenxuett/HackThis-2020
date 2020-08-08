bcf540698b2650583dc9f74364f8a99e185ae6aa
goldenxuett <ttanpcs@gmail.com>
2020-08-08 16:36:02 -0500

    Configure base Home, About, and Main page

--- omegalul/src/App.js
+++ omegalul/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Main from './pages/Main';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* A <Switch> looks through its children <Route>s and renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/main">
+          <Main />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ omegalul/src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { withRouter } from 'react-router-dom';
+import "./style.sass"
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="home-page">
+        
+      </div>
+      )
+  }
+}
+
+
+export default withRouter(Home)
